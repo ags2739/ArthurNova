1da98854e2986887fd0bc567cc70021f7100d628
Arthur Smith <ags2739@email.vccs.edu>
2015-02-25 17:47:06 -0500

    Adding Assignment2table.java

---
+++ Assignment2table.java
@@ -0,0 +1,12 @@
+public class Assignment2table {
+	public static void main (String [] args) {
+		//Calculates several expressions and displays their output.
+		System.out.println("1/2=" + (1/2) + " - Data Type: Int");
+		System.out.println("1%2=" + (1%2) + " - Data Type: Int");
+		System.out.println("1.0/2=" + (1.0/2) + " - Data Type: double");		System.out.println("5+7/2=" + (5+7/2) + " - Data Type: int");
+		System.out.println("Beat" + ' ' + "Army" + "- Data Type: string");
+		System.out.println("6+13/5-35%3=" + (6+13/5-35%3) + " - Data Type: int");
+		System.out.println("3.5*(5/4)=" + (3.5*(5/4)) + " - Data Type: double");
+		System.out.println("(3.5*5)/4=" + ((3.5*5)/4) + " - Data type: double");
+	}
+}
