51d9828047530f7d81712614dec0b99a6e2d5e5e
Arthur Smith <ags2739@email.vccs.edu>
2015-03-26 16:42:15 -0400

    Adding OddOutput.java

---
+++ documents/JavaFiles/OddOutput.java
@@ -0,0 +1,14 @@
+
+public class OddOutput {
+
+	public static void main(String[] args) {
+		int oddNumbers = -1;
+		
+		for (int i = 0; i <=100; i++) {
+			oddNumbers = (oddNumbers + 2);
+			System.out.println(oddNumbers);
+		}
+
+	}
+
+}
